abe10529f6b33c06d9cfa3365b8f18bda5893e60
Casey Chance <caseyjchance@gmail.com>
2017-03-29 23:51:15 -0700

    use sfx api

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from cgi import escape
+from requests import get
 
 from .models import Greeting
 
@@ -8,11 +9,16 @@ from .models import Greeting
 def index(request):
     name = escape(request.META['HTTP_HOST']).split('.')[-3]
     title = "%s got peachd" % (name.title())
+    try:
+        fart = get("https://sfx-api.herokuapp.com/farts/random").json()
+    except:
+        fart = {}
 
     return render(request, 'index.html', {
         'greeting': {
             'title': title,
-            'name': name
+            'name': name,
+            'fart': fart
         }
     })
 
